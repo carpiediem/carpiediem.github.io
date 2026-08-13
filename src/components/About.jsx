7583f4e4c93d726632285a3348eddbdb40576368
Ryan Carpenter <carpiediem@gmail.com>
2026-08-13 15:57:11 -0400

    Initial cleanup for 2026

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,27 @@
 /* global ga */
 
-import React, { forwardRef } from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Grid from '@material-ui/core/Grid';
-import Icon from '@material-ui/core/Icon';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Tooltip from '@material-ui/core/Tooltip';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GitHubIcon from '@material-ui/icons/GitHub';
+import React, { forwardRef } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Grid from "@material-ui/core/Grid";
+import Icon from "@material-ui/core/Icon";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Tooltip from "@material-ui/core/Tooltip";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
-import Introduction from './Introduction';
+import Introduction from "./Introduction";
 
 const ColorTooltip = withStyles((theme) => ({
   tooltip: {
     backgroundColor: theme.palette.primary.light,
-    color: 'rgba(0, 0, 0, 0.87)',
+    color: "rgba(0, 0, 0, 0.87)",
     boxShadow: theme.shadows[1],
     fontSize: 11,
     marginTop: 0,
@@ -32,63 +32,63 @@ const ColorTooltip = withStyles((theme) => ({
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 960,
-    padding: '0 15px',
-    margin: 'auto',
+    padding: "0 15px",
+    margin: "auto",
   },
   background: {
-    backgroundSize: 'cover',
-    backgroundColor: '#242832',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    backgroundImage: 'url(img/background.jpg)',
-    filter: 'brightness(30%)',
+    backgroundSize: "cover",
+    backgroundColor: "#242832",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundImage: "url(img/background.jpg)",
+    filter: "brightness(30%)",
     minHeight: 515,
-    width: '100%',
-    position: 'absolute',
+    width: "100%",
+    position: "absolute",
     top: 0,
     zIndex: -2,
   },
   fadeToWhite: {
     minHeight: 515,
-    width: '100%',
-    position: 'absolute',
+    width: "100%",
+    position: "absolute",
     top: 0,
     zIndex: -1,
-    background: 'rgb(255,255,255)',
+    background: "rgb(255,255,255)",
     // eslint-disable-next-line no-dupe-keys
     background:
-      'linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 40%, rgba(255,255,255,0) 100%)',
+      "linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 40%, rgba(255,255,255,0) 100%)",
   },
   card: {
-    margin: '100px auto 0 auto',
+    margin: "100px auto 0 auto",
     minWidth: 275,
     borderRadius: 0,
-    border: 'none',
-    '& a': { textDecoration: 'none', color: 'inherit' },
+    border: "none",
+    "& a": { textDecoration: "none", color: "inherit" },
   },
-  content: { padding: '30px 20px 15px 20px', textAlign: 'center' },
-  photo: { maxWidth: '100%' },
+  content: { padding: "30px 20px 15px 20px", textAlign: "center" },
+  photo: { maxWidth: "100%" },
   item: {
-    width: 'inherit',
-    color: 'rgb(61, 68, 81)',
-    '& a': { outline: 'none' },
+    width: "inherit",
+    color: "rgb(61, 68, 81)",
+    "& a": { outline: "none" },
   },
   list: {
-    '& li': {
-      padding: '4px 0',
-      '& span.MuiListItemText-primary': {
+    "& li": {
+      padding: "4px 0",
+      "& span.MuiListItemText-primary": {
         fontSize: 12,
         fontWeight: 700,
-        textTransform: 'uppercase',
+        textTransform: "uppercase",
       },
-      '& p.MuiListItemText-secondary': {
-        color: '#9da0a7',
+      "& p.MuiListItemText-secondary": {
+        color: "#9da0a7",
         fontSize: 15,
         fontWeight: 400,
-        '& a': { color: 'inherit', textDecoration: 'none' },
+        "& a": { color: "inherit", textDecoration: "none" },
       },
-      '@media (min-width: 600px)': {
-        '& p.MuiListItemText-secondary': {
+      "@media (min-width: 600px)": {
+        "& p.MuiListItemText-secondary": {
           marginTop: -20,
           marginLeft: 100,
         },
@@ -97,33 +97,33 @@ const useStyles = makeStyles((theme) => ({
   },
   actions: {
     backgroundColor: theme.palette.primary.main,
-    justifyContent: 'center',
-    '& a': {
-      color: 'white',
-      '& svg': { fill: 'white' },
+    justifyContent: "center",
+    "& a": {
+      color: "white",
+      "& svg": { fill: "white" },
     },
   },
   download: {
-    color: 'rgb(61, 68, 81)',
+    color: "rgb(61, 68, 81)",
     fontSize: 15,
     fontWeight: 700,
     marginBottom: 30,
-    padding: '15px 55px',
-    '&:hover': {
+    padding: "15px 55px",
+    "&:hover": {
       boxShadow:
-        '0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15)',
+        "0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15)",
     },
   },
-  hello: { color: 'black', fontSize: 20, fontWeight: 300 },
+  hello: { color: "black", fontSize: 20, fontWeight: 300 },
 }));
 
 export default forwardRef((props, ref) => {
   const classes = useStyles();
 
   const triggerDownloadEvent = () =>
-    ga('send', 'event', 'File', 'Download', 'Resume');
+    ga("send", "event", "File", "Download", "Resume");
   const triggerEmailEvent = () =>
-    ga('send', 'event', 'Link', 'Click', 'Email Address');
+    ga("send", "event", "Link", "Click", "Email Address");
 
   return (
     <React.Fragment>
@@ -141,14 +141,14 @@ export default forwardRef((props, ref) => {
                   name={
                     <React.Fragment>
                       {/* <span className="thin">I'm </span> */}
-                      Ryan{' '}
+                      Ryan{" "}
                       <ColorTooltip
                         arrow
                         title="Scott Luong"
                         placement="bottom"
                       >
                         <span>SL</span>
-                      </ColorTooltip>{' '}
+                      </ColorTooltip>{" "}
                       Carpenter
                     </React.Fragment>
                   }
@@ -158,7 +158,7 @@ export default forwardRef((props, ref) => {
                   <ListItem>
                     <ListItemText
                       primary="Address"
-                      secondary="Hong Kong"
+                      secondary="Denver, CO, USA"
                     ></ListItemText>
                   </ListItem>
                   <ListItem>
@@ -217,7 +217,7 @@ export default forwardRef((props, ref) => {
               variant="outlined"
               size="large"
               component="a"
-              href="/downloads/RyanSLCarpenter.pdf"
+              href="/downloads/Resume-Ryan-SL-Carpenter.pdf"
               className={classes.download}
               onClick={triggerDownloadEvent}
             >
