01e257fc5d99f70526896c2a0041f13f67cada8f
Ryan Carpenter <carpiediem@gmail.com>
2026-08-17 09:46:54 -0600

    Images and placeholders

--- src/screens/Project.jsx
+++ src/screens/Project.jsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     padding: 25,
     margin: 'auto',
   },
-  img: { maxWidth: '100%' },
+  img: { maxWidth: "100%", maxHeight: "80vh" },
   title: { fontSize: 30, fontWeight: 700 },
   content: {
     color: '#666',
