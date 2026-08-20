15715b1ffccf2c201c454aba421a01a22e77ae5c
Ryan Carpenter <carpiediem@gmail.com>
2026-08-19 19:39:09 -0600

    cleanup

--- src/screens/Project.jsx
+++ src/screens/Project.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useParams, Redirect } from "react-router-dom";
 import debounce from "lodash/debounce";
 import { styled } from "@mui/material/styles";
-// import ButtonGroup from '@mui/material/ButtonGroup';
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
